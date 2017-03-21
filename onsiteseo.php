bd24092364be51aeb801b28bb6638055cf7439e6
dmsbilas <dmsbilas@yahoo.com>
2017-03-21 11:38:43 +0600

    initial commit with full project

---
+++ onsiteseo.php
@@ -0,0 +1,73 @@
+<?php
+/*
+Plugin Name: On site seo
+Plugin URI: http://haidersiddiq.info/wp-content/uploads/2017/03/onsiteseo.zip
+Description: You can add meta keywords into your wordpress page
+Version: 1.0
+Author: Bilas Siddiq
+Author URI: http://haidersiddiq.info
+License: GPL2
+*/
+
+class OnsiteSeo
+{
+    private static $PLUGIN_URL;
+    public function OnsiteSeo()
+    {
+        $this->PLUGIN_URL = plugin_dir_path( __FILE__ );
+        add_action('wp_head', array(&$this, 'AddMetaTags'));
+        add_action('admin_menu',array(&$this,'SalsabeelKeywordsMenu'));
+        add_action('wp_ajax_add_keywords_into_page',array(&$this,'AddKeywordsIntoPage'));
+        add_action('wp_ajax_get_keywords_by_page_slug',array(&$this,'GetKeywordsByPageSlug'));
+    }
+    public function AddMetaTags()
+    {
+        global $post;
+        $metakeywords = get_post_meta($post->ID, $post->post_name, true);
+        echo '<meta name="keywords" content="' . $metakeywords . '" />' . "\n";
+    }
+    
+    public function SalsabeelKeywordsMenu()
+    {
+        add_menu_page( 'Sal Sabeel Keywords', 'Sal Sabeel Keywords', 'manage_options', 'add_keywords',array(&$this,'AddKeywordsPage'));
+    }
+    
+    public function AddKeywordsPage()
+    {
+        include_once($this->PLUGIN_URL."/template/insert_keywords.php");
+    }
+    
+    public function AddKeywordsIntoPage()
+    {
+        $page_slug = $_POST['page_slug'];
+        $page_title = $_POST['page_title'];
+        $keywords = $_POST['keywords'];
+        $page_id     = &$this->get_id_by_slug($page_slug);
+        $_POST['page_id'] = $page_id;
+        update_post_meta($page_id, $page_slug, $keywords);
+        echo json_encode($_POST);
+        exit();
+    }
+    
+    public function get_id_by_slug($page_slug) 
+    {
+        $page = get_page_by_path($page_slug);
+        if ($page) {
+            return $page->ID;
+        } else {
+            return null;
+        }
+    }
+
+    public function GetKeywordsByPageSlug()
+    {
+        $page_slug = $_POST['page_slug'];
+        $page_id = &$this->get_id_by_slug($page_slug);
+        $keywords = get_post_meta($page_id, $page_slug, true);
+        echo json_encode($keywords);
+        exit();
+    }
+}//End of class OnsiteSeo;
+
+$wpOnsiteSeo = new OnsiteSeo();
+?>
---
+++ template/insert_keywords.php
@@ -0,0 +1,109 @@
+<?php
+$args = array(
+'post_type' => 'page',
+'post_status' => 'publish'
+);
+$pages = get_pages($args);
+?>
+
+  <table>
+  <hr />
+    <tr>
+      <td>
+        <select id="page_selector" name="pagename">
+          <?php foreach($pages as $key => $value){?>
+            <option value="<?php echo $value->post_name ?>">
+              <?php echo $value->post_title ?>
+            </option>
+            <?php } ?>
+        </select>
+      </td>
+      <td>
+        <textarea id="keywords" rows="5" cols="65" name="keywords" value="">
+          <?php echo trim(get_post_meta($value->ID, $value->post_name, true)); ?>
+        </textarea>
+      </td>
+      <td>
+        <span id="loader" hidden><img style="width:100px; height:100px;opacity:.5;border-radius:60px" src="<?php echo plugins_url( '../images/65.gif', __FILE__ );?>"> </span>
+      </td>
+      <td>
+        <button class="btn_save_keywords"> Save Keywords </button>
+      </td>
+    </tr>
+  </table>
+
+  <hr/>
+
+  <table style="width:100%">
+    <tr style="text-align:left;">
+      <th style="min-width: 200px"><strong>Page Name</strong></th>
+      <th><strong>Keywords</strong></th>
+    </tr>
+    <?php foreach ($pages as $key => $value) { ?>
+      <tr id="<?php echo $value->post_name; ?>">
+        <td>
+          <?php echo $value->post_title; ?>
+        </td>
+        <td>
+          <?php echo get_post_meta($value->ID,$value->post_name, true); ?>
+        </td>
+      </tr>
+      <?php } ?>
+
+
+  </table>
+
+  <script type="text/javascript">
+    jQuery(document).ready(function($) {
+      $('#page_selector').on('change', function() {
+        $('#loader').show();
+        $('#keywords').hide();
+        var page_slug = $(this).val();
+        var page_title = $('#page_selector option:selected').text();
+        var ajax_url = "<?php echo admin_url('admin-ajax.php') ?>";
+
+        jQuery.ajax({
+          url: ajax_url,
+          type: 'post',
+          data: {
+            action: 'get_keywords_by_page_slug',
+            page_slug: page_slug,
+            page_title: page_title.trim()
+          },
+          success: function(response) {
+            $('#keywords').val($.parseJSON(response));
+            // console.log(response);
+            $('#loader').hide();
+            $('#keywords').show();
+          }
+        });
+
+      });
+
+      $('.btn_save_keywords').on('click', function() {
+        var page_slug = $('#page_selector').val();
+        var page_title = $('#page_selector option:selected').text().trim();
+        var keywords = $('#keywords').val().trim();
+        var ajax_url = "<?php echo admin_url('admin-ajax.php') ?>";
+        $('#loader').show();
+        $('#keywords').hide();
+        jQuery.ajax({
+          url: ajax_url,
+          type: 'post',
+          data: {
+            action: 'add_keywords_into_page',
+            page_slug: page_slug,
+            page_title: page_title,
+            keywords: keywords
+          },
+          success: function(response) {
+            $('tr#' + page_slug).html(
+              "<td>" + page_title.trim() + "</td><td>" + keywords.trim() + "</td>"
+            );
+            $('#loader').hide();
+            $('#keywords').show();
+          }
+        });
+      });
+    });
+  </script>
