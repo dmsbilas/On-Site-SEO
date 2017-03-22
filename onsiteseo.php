3c937f96dc73564abd65891b09218c15625ac75f
dmsbilas <dmsbilas@yahoo.com>
2017-03-22 17:05:43 +0600

    added security issues

--- onsiteseo.php
+++ onsiteseo.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Plugin Name: On site seo
-Plugin URI: http://haidersiddiq.info/wp-content/uploads/2017/03/onsiteseo.zip
+Plugin URI: http://haidersiddiq.info/wp-content/uploads/2017/03/On-Site-SEO.zip
 Description: You can add meta keywords into your wordpress page
 Version: 1.0
 Author: Bilas Siddiq
@@ -16,7 +16,7 @@ class OnsiteSeo
     {
         $this->PLUGIN_URL = plugin_dir_path( __FILE__ );
         add_action('wp_head', array(&$this, 'AddMetaTags'));
-        add_action('admin_menu',array(&$this,'SalsabeelKeywordsMenu'));
+        add_action('admin_menu',array(&$this,'KeywordsMenu'));
         add_action('wp_ajax_add_keywords_into_page',array(&$this,'AddKeywordsIntoPage'));
         add_action('wp_ajax_get_keywords_by_page_slug',array(&$this,'GetKeywordsByPageSlug'));
     }
@@ -27,7 +27,7 @@ class OnsiteSeo
         echo '<meta name="keywords" content="' . $metakeywords . '" />' . "\n";
     }
     
-    public function SalsabeelKeywordsMenu()
+    public function KeywordsMenu()
     {
         add_menu_page( 'SEO Keywords', 'SEO Keywords', 'manage_options', 'add_keywords',array(&$this,'AddKeywordsPage'));
     }
@@ -39,18 +39,31 @@ class OnsiteSeo
     
     public function AddKeywordsIntoPage()
     {
-        $page_slug = $_POST['page_slug'];
-        $page_title = $_POST['page_title'];
-        $keywords = $_POST['keywords'];
-        $page_id     = &$this->get_id_by_slug($page_slug);
-        $_POST['page_id'] = $page_id;
-        update_post_meta($page_id, $page_slug, $keywords);
-        echo json_encode($_POST);
-        exit();
+        $data = array();
+        $page_slug = (isset($_POST['page_slug']) && $_POST['page_slug'] !='') ? sanitize_text_field(strip_tags($_POST['page_slug'])) : "" ;
+        $page_title = (isset($_POST['page_title']) && $_POST['page_title'] !='') ?  sanitize_text_field(strip_tags($_POST['page_title'])) : "";
+        $keywords = (isset($_POST['keywords']) && $_POST['keywords'] !='') ?  sanitize_text_field(strip_tags($_POST['keywords'])) : "";
+        if($page_slug != '' && $page_title !='')
+        {
+            $page_id                = &$this->get_id_by_slug($page_slug);
+            $data['page_slug']      = $page_slug;
+            $data['page_title']     = $page_title;
+            $data['keywords']       = $keywords;
+            $data['page_id']        = $page_id;
+            update_post_meta($page_id, $page_slug, $keywords);
+            $data['message']        = "success";
+            echo json_encode($data);
+            exit();
+        }else{
+            $data['message']        = "fail";
+            exit();
+        }
+        
     }
     
     public function get_id_by_slug($page_slug) 
     {
+        if($page_slug == '') return null;
         $page = get_page_by_path($page_slug);
         if ($page) {
             return $page->ID;
@@ -61,13 +74,13 @@ class OnsiteSeo
 
     public function GetKeywordsByPageSlug()
     {
-        $page_slug = $_POST['page_slug'];
-        $page_id = &$this->get_id_by_slug($page_slug);
-        $keywords = get_post_meta($page_id, $page_slug, true);
+        $page_slug      = (isset($_POST['page_slug']) && $_POST['page_slug'] !='') ? sanitize_text_field(strip_tags($_POST['page_slug'])) : "" ;
+        $page_id        = &$this->get_id_by_slug($page_slug);
+        $keywords       = get_post_meta($page_id, $page_slug, true);
         echo json_encode($keywords);
         exit();
     }
 }//End of class OnsiteSeo;
-
+if ( ! defined( 'ABSPATH' ) ) exit; 
 $wpOnsiteSeo = new OnsiteSeo();
 ?>
--- template/insert_keywords.php
+++ template/insert_keywords.php
@@ -1,4 +1,6 @@
 <?php
+if ( ! defined( 'ABSPATH' ) ) exit; 
+
 $args = array(
 'post_type' => 'page',
 'post_status' => 'publish'
